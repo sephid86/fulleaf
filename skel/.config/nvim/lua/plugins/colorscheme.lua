2644c52995e0ad804f2b98c4e58ef8b9efdbf2a6
sephid86 <sephid86@gmail.com>
2025-11-29 16:51:07 +0900

    251129

---
+++ skel/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      -- 다른 스타일 설정들...
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
